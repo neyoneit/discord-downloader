2b119fa3876f4d3e9805622d4ee747522eebe559
Vít Šesták <v6ak@users.noreply.github.com>
2021-11-13 20:44:07 +0100

    Fix youtube-refresh.py

--- youtube-refresh.py
+++ youtube-refresh.py
@@ -6,5 +6,6 @@ from settings import DEMO_RENDERING_LOCAL_YOUTUBE_EXECUTABLE, DEMO_RENDERING_LOC
 subprocess.call([
     DEMO_RENDERING_LOCAL_YOUTUBE_EXECUTABLE,
     *DEMO_RENDERING_LOCAL_YOUTUBE_PARAMS,
+    '--title=je-to-uplne-jedno-ale-neco-tu-musi-byt',
     '--refresh-auth'
 ])
